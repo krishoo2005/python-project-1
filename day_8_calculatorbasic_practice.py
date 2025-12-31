8c404fa0975a747e067d565e3647c9fbb612bb00
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2025-12-31 12:19:59 +0530

    Add files via upload

---
+++ day_8_calculatorbasic_practice.py
@@ -0,0 +1,17 @@
+#day 8 python basic calculator 
+print("==KRUSHNA CALCULATOR ")
+
+def calculator():
+    num1 = 30
+    num2 = 7
+
+    print(f"number 1 :{num1}")
+    print(f"number 2 :{num2}")
+    print(f"addition: {num1 + num2}")
+    print(f"subtractions: {num1 - num2}")
+    print(f"mutiplication:{num1 * num2}\n")
+
+
+
+calculator()
+
