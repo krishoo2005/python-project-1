0bc599a38e81f7cd201e5206813005854d78ceb3
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-07 18:12:55 +0530

    Add files via upload

---
+++ day1_array_additon.py
@@ -0,0 +1,8 @@
+arr1 = [1, 2, 3, 4 ]
+arr2 = [5, 6, 7, 8]
+
+result = []
+
+for i in range(len(arr1)):
+    result.append(arr1[i]+arr2[i])
+print("resultanat array",result)
---
+++ day1_array_max_min.py
@@ -0,0 +1,7 @@
+numbers = [10,11,12,13,21,45,76,98]
+max_num = numbers[0]
+for num in numbers:
+    if num > max_num:
+       max_num = num
+print("maximum number is :",max_num)
+
