3dd0a72e00d0f38cedce67e6b8fe0df4d92269d8
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-10 19:39:03 +0530

    tuple basic

---
+++ day_17_tuple.py
@@ -0,0 +1,22 @@
+#learning tuple 
+#unmutable in tuple value cant change after write in variable .()
+# a = (1,1,12,12,15,55,12)
+# print(type(a))
+
+
+# # tuple = () index print value in line 
+
+# k = (23,23,234,42,1,313,13,2,442)
+# for i in range(len(k)):
+#     print(k[i])
+
+
+#tuple print count that number how many times repeated 
+# k = (23,23,234,42,1,313,13,2,442,1,1,1,1,11,1,1,1,1,1,1,1,1,1,1)
+# count = k.count(42)
+# print(count)
+
+#tuple  unpacking
+
+# a,b,c,d = (1,2,3,4)
+# print(d)
