283219a039f8bf5167f3dc86e67ac7f2145c4db9
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-10 19:38:44 +0530

    set practice

---
+++ day_17_set_programs.py
@@ -0,0 +1,60 @@
+#set 
+#set mai hashing form mai dataimfo save hota hai
+#mutable you can change the value of variable 
+#no duplicate allow i mean if you write same number again 
+# and agiain it only take one number from repeated 
+#indexed  value not access
+#unorderd har baar hash value alag milti hai 
+
+
+# s = { 1,2,3,3,4,54}
+# print(s[0]) not runnn
+
+# s = { 1,2,3,3,4,54}
+# print(s)  it will run and give all number in output 
+
+# it has method to add remove discard the values
+# a.clear()
+# a.add()
+# a.discard()
+# a.remove()
+#a.union() or a|b
+# a.intersction  or (a & b)
+# a.difference(b) or (a-b)
+# a.symmetric_difference(b) or (a^b)
+
+
+# a = {1,2,3,4,5}
+# s = a.clear()
+# print(a)
+
+
+# a = {1,2,3,4,5}
+# s = a.add(7)
+# print(a)
+
+# a = {1,2,3,4,5,6}
+# s = a.discard(3)
+# print(a)
+
+
+
+# a = {1,2,3,100,2}
+# b = {1,2,3,5,8,}
+# s = a.union(b)
+# print(s)
+
+# or
+
+# a = {1,2,3,100,2}
+# b = {1,2,3,5,8,}
+# s = a|b
+# print(s)
+
+
+# a = {1,19,2,3,100,2,4}
+# b = {1,2,3,5,4,8,}
+# s = (a & b)
+# print(s)
+
+
