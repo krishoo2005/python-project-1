5bf4a586bc60b78fa328c8c27a3b22e5296d28d1
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-12 21:05:20 +0530

    string operation

---
+++ day_19_basic string operations.py
@@ -0,0 +1,8 @@
+s = "hello world"
+
+print(len(s))          # length
+print(s[0])            # first character
+print(s[-1])           # last character
+print(s.upper())       # uppercase
+print(s.lower())       # lowercase
+print(s[::-1])         # reverse string (important)
