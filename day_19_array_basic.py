a93f2e44eabfa2c91c85f045fc5146889ba3f7d2
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-12 21:10:59 +0530

    arrays basic

---
+++ day_19_array_basic.py
@@ -0,0 +1,18 @@
+# create array (list)
+arr = [10, 20, 30, 40]
+
+# access
+print(arr[0])     # first element
+
+# update
+arr[1] = 25
+
+# add element
+arr.append(50)
+
+# delete element
+arr.remove(30)
+
+# traverse
+for x in arr:
+    print(x)
