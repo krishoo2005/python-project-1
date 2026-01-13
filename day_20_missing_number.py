ffde1bcc2b8d80bce5dfe3401aa4c3fea30e84c2
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-13 17:18:48 +0530

    finding missing number using for loop

---
+++ day_20_missing_number.py
@@ -0,0 +1,12 @@
+#using loop missing number program
+#for loop
+
+arr = [1, 2, 3 , 4 , 5 , 7]
+n = 7
+
+for i in range(1, n + 1):
+    if i not in arr:
+        print(i)
+        break
+
+#this is of array question
