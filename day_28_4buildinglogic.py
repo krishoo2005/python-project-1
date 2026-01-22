14fd4b662dbc0c7aced31687b0eaaf42488b561d
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-22 15:22:41 +0530

    count numbers

---
+++ day_28_4buildinglogic.py
@@ -0,0 +1,21 @@
+#digit counts program 
+#logic building practicing this programs 
+n = 12145
+
+digit = 0
+
+while n > 0:
+    digit = digit +1
+
+    n = n // 10
+print(f"numbers are {digit}")
+
+
+
+n = 12013
+count =0
+while n > 0 :
+    count = count +1
+    n = n // 10
+
+print(f"numbers total are {count}")    
