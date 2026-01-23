bd1625680f47692d007015ee28019469c0f9a758
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-01-23 16:35:20 +0530

    Add files via upload

---
+++ day_28_5buildingLogic.py
@@ -0,0 +1,11 @@
+#largest number finding
+n = 705852
+type(n)
+# largest  = 0
+
+# while n > 0 :
+#     digit = n % 10
+#     if digit > largest:
+#         largest = digit
+#     n = n // 10
+# print("largest number is", largest)        
