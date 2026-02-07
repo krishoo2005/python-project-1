58151d13fa97fa5dd1eba95a3bb61fa8bf5bdb7f
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-02-07 23:03:05 +0530

    Add files via upload

---
+++ day_39_26_BuildingLogic.py
@@ -0,0 +1,9 @@
+# Program 2: Sort characters (ascending)
+arr = ['z', 'a', 'd', 'b']
+
+for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+        if arr[i] > arr[j]:
+            arr[i], arr[j] = arr[j], arr[i]
+
+print(arr)
