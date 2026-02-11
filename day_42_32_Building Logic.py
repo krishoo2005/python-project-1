2d3a60c657d14f26f8c571289365ed3bb7273817
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-02-11 21:18:47 +0530

    Add files via upload

---
+++ day_42_32_Building Logic.py
@@ -0,0 +1,19 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[0]
+    left = []
+    right = []
+
+    for i in arr[1:]:
+        if i <= pivot:
+            left.append(i)
+        else:
+            right.append(i)
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+arr = ['d', 'a', 'c', 'b']
+print(quick_sort(arr))
